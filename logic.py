c30eaa09444a7cf20d147aeb6c4ec82caf023965
Nazar Korchizhinskiy <korchizhinskiy@gmail.com>
2022-05-01 20:39:28 +0700

    Add painter line, ellipse, rectangle, signals for change radiobuttons

--- logic.py
+++ logic.py
@@ -1,3 +1,4 @@
+from tkinter import Button
 from PyQt5.QtWidgets import QMainWindow,\
                             QRadioButton,\
                             QComboBox,\
@@ -6,6 +7,12 @@ from PyQt5.QtWidgets import QMainWindow,\
                             QLabel,\
                             QWidget,\
                             QDialog
+from PyQt5.QtGui import QPainter, \
+                        QPixmap, \
+                        QColor
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt, \
+                         QPoint
 
 
 
@@ -44,7 +51,13 @@ class MainWindow(QDialog):
         self.combobox_background = QComboBox(self)
         self.combobox_fill = QComboBox(self)
         
-        self.board = Board()
+        self.board = Board(
+            self.radiobutton_line,
+            self.radiobutton_ellipse,
+            self.radiobutton_rectangle,
+            self.combobox_line,
+            self.combobox_fill
+        )
 
         
     def _set_layouts(self):
@@ -88,16 +101,91 @@ class MainWindow(QDialog):
         print(f"Change to {value}")
         
     def _change_background(self, value):
-        self.board.setStyleSheet(f"background-color: {value};")
+        self.board.pixmap.fill(QColor(value))
+        self.board.update()
         
     def _change_fill(self, value):
         print(f"Change to {value}")
+
+    #// QPainter.
+        
+    
                
 
                
 
 class Board(QDialog):
     
-    def __init__(self):
+    def __init__(self, radiobutton_line, radiobutton_ellipse, radiobutton_rectangle,
+                 combobox_line, combobox_fill):
         super().__init__()
         self.setMinimumWidth(700)
+        self.point_start = 0
+        self.point_end = 0
+        self.pixmap = QPixmap(700, 600)
+        self.pixmap.fill(Qt.white)
+        self.points = []
+
+        #// Init
+        self.radiobutton_line = radiobutton_line
+        self.radiobutton_ellipse = radiobutton_ellipse
+        self.radiobutton_rectangle = radiobutton_rectangle
+        self.combobox_line = combobox_line
+        self.combobox_fill = combobox_fill
+        
+    def paintEvent(self, event):
+        self.painter = QPainter()
+        self.painter.begin(self.pixmap)
+        if self.point_start != 0 and self.point_end != 0:
+            print(self.points)
+            for figure in self.points:
+                if figure[0] == "line":
+                    self.painter.drawLine(figure[1], figure[2])
+
+                elif figure[0] == "ellipse":
+                    x_center = figure[1].x()
+                    y_center = figure[1].y()
+                    r_x = figure[2].x() - figure[1].x()
+                    r_y = figure[2].y() - figure[1].y()
+                    self.painter.drawEllipse(x_center, y_center, r_x, r_y)
+                
+                elif figure[0] == "rectangle":
+                    self.painter.drawRect(figure[1].x(), figure[1].y(), 
+                                          figure[2].x() - figure[1].x(), 
+                                          figure[2].y() - figure[1].y())
+                
+        self.painter.end()
+
+        self.painter_2 = QPainter()
+        self.painter_2.begin(self)
+        self.painter_2.drawPixmap(0, 0, self.pixmap)
+        self.painter_2.end()
+        
+    def mousePressEvent(self, event):
+        if self.radiobutton_line.isChecked():
+            if event.buttons() == QtCore.Qt.LeftButton:
+                self.point_start = event.pos()
+        elif self.radiobutton_ellipse.isChecked():
+            if event.buttons() == QtCore.Qt.LeftButton:
+                self.point_start = event.pos()
+        elif self.radiobutton_rectangle.isChecked():
+            if event.buttons() == QtCore.Qt.LeftButton:
+                self.point_start = event.pos()
+            
+
+    def mouseReleaseEvent(self, event):
+        def update_dict_with_points(figure):
+            if event.button() == QtCore.Qt.LeftButton:
+                self.point_end = event.pos()
+                self.points.append([f"{figure}", self.point_start, self.point_end])
+                self.update()
+
+        if self.radiobutton_line.isChecked():
+            print('1')
+            update_dict_with_points("line")
+        elif self.radiobutton_ellipse.isChecked():
+            print('2')
+            update_dict_with_points("ellipse")
+        elif self.radiobutton_rectangle.isChecked():
+            print('3')
+            update_dict_with_points("rectangle") 
